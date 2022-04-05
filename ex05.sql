32c46150ed58817ac82ad372bc74f4a5189b6d76
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2022-04-05 20:20:26 -0300

    Add files via upload

---
+++ ex05.sql
@@ -0,0 +1,21 @@
+--1
+SELECT 'O identificador da ' || ds_funcao || ' é o id ' || id_funcao AS "Descrição das funções"
+FROM tb_funcao;
+
+--2
+SELECT 'circunferência ' || (22/7) * (6000 * 6000) AS "Área" FROM dual;
+
+--3
+SELECT nm_departamento FROM tb_departamento
+WHERE nm_departamento LIKE '%ing';
+
+--4
+SELECT ds_funcao, base_salario, teto_salario - base_salario
+FROM tb_funcao
+WHERE ds_funcao = 'Presidente' OR ds_funcao = 'Gerente'
+ORDER BY (teto_salario - base_salario) DESC;
+
+--5
+&&v
+SELECT id_empregado, nome, salario, salario*12, &v AS "aliquota",  &v * (salario*12) AS "aliquote anual"
+FROM tb_empregado;
